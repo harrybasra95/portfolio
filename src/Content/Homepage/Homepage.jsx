ff5b1714ff7fc923014680b360a9d8a76df000e9
Hanarinder Singh <harnarinder.singh@yum.com>
2022-10-18 23:58:27 +0530

    feat: created profile container working on sub content container

--- src/Content/Homepage/Homepage.jsx
+++ src/Content/Homepage/Homepage.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import './Homepage.scss';
-import Header from 'Components/Header/Header';
 import MainLayout from 'Layouts/Main';
 import BackgroundPattern from 'Components/BackgroundPattern/BackgroundPattern';
+import ProfileImg from 'Images/profile-img.png';
 
 const Homepage = () => {
      return (
-          <div className="homepage-container">
-               <BackgroundPattern></BackgroundPattern>
-               <MainLayout>
-                    <Header />
-               </MainLayout>
-          </div>
+          <MainLayout>
+               <div className="homepage-container">
+                    <BackgroundPattern></BackgroundPattern>
+                    <div className="main-content-container">
+                         <div className="profile-container">
+                              <img src={ProfileImg} alt="" />
+                         </div>
+                         <div className="heading-title">
+                              <div className="main-heading">
+                                   <h1>
+                                        👋 Hey there,
+                                        <br />
+                                        It&apos;s Harry from India
+                                   </h1>
+                              </div>
+                              <div className="sub-heading">
+                                   <h2>Sr. Full Stack Developer.</h2>
+
+                                   <div className="skills-container">
+                                        <p>Node JS</p>
+                                        <p>React JS</p>
+                                        <p>Solidity</p>
+                                        <p>DevOps</p>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </MainLayout>
      );
 };
 
--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from 'Components/Header/Header';
 import './Main.scss';
 
 const MainLayout = ({ children }) => {
-     return <div className="main-layout">{children}</div>;
+     return (
+          <div className="main-layout">
+               <Header />
+               {children}
+          </div>
+     );
 };
 
 MainLayout.propTypes = {
