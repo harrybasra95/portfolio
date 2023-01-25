28cbee1e6c42eafea4b6a5d854c7f30cf25e8af5
Harnarinder Singh <harry.basra95@gmail.com>
2023-01-25 12:00:12 +0530

    jenkins

--- scanner.js
+++ scanner.js
@@ -1,17 +1,17 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner(
-     {
-          serverUrl: 'http://35.154.113.233:9000',
-          options: {
-               'sonar.sources': 'src',
-               'sonar.projectKey': 'Portfolio',
-               'sonar.tests': 'src',
-               'sonar.login': 'sqp_c59558b31be350d6851fa3db4cbd3aecf64cb6b6',
-               'sonar.inclusions': '**', // Entry point of your code
-               'sonar.test.inclusions':
-                    'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
-          },
-     },
-     () => {}
+ {
+  serverUrl: 'http://35.154.113.233:9000',
+  options: {
+   'sonar.sources': 'src',
+   'sonar.projectKey': 'Portfolio',
+   'sonar.tests': 'src',
+   'sonar.login': 'sqp_f472ff564e60a13df1d9e90bfe32378da4944cd9',
+   'sonar.inclusions': '**', // Entry point of your code
+   'sonar.test.inclusions':
+    'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+  },
+ },
+ () => {}
 );
